f450218790967f0a55e513e2388f92df2006a855
Aditya Dadhich <adidadhich2003@gmail.com>
2025-03-26 19:33:29 +0530

    audio_processing

--- frontend/src/pages/VideoRoom.jsx
+++ frontend/src/pages/VideoRoom.jsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
+
+
+
+
 // get token
 async function generateToken(tokenServerUrl, userID) {
   // Obtain the token interface provided by the App Server
@@ -32,6 +36,58 @@ function getUrlParams(url) {
 }
 
 export default function VideoRoom() {
+
+  const [recording, setRecording] = React.useState(false);
+  const [recordingURL, setRecordingURL] = React.useState(null);
+  const audioChunks = React.useRef([]);
+  const mediaRecorder = React.useRef(null);
+  const [hasJoinedRoom, setHasJoinedRoom] = React.useState(false);
+
+  const startRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaRecorder.current = new MediaRecorder(stream);
+      mediaRecorder.current.ondataavailable = (e) => {
+        audioChunks.current.push(e.data);
+      };
+
+      mediaRecorder.current.onstop = () => {
+        const audioBlob = new Blob(audioChunks.current, {
+          type: 'audio/wav',
+        });
+        const url=URL.createObjectURL(audioBlob)
+        
+        downLoadRecording(url)
+      }
+      mediaRecorder.current.start();
+      setRecording(true);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const endRecording = () => {
+    if (mediaRecorder.current && mediaRecorder.current.state === 'recording') {
+      
+      mediaRecorder.current.stop();
+      setRecording(false);
+    }
+  }
+
+  const downLoadRecording = (url) => {
+    if (url) {
+      console.log(url);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `meeting-recording-${new Date().toISOString()}.wav`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  }
+
+
+
   const roomID = getUrlParams().get('roomID') || randomID(5);
   let myMeeting = async (element) => {
     const userID = randomID(5);
@@ -48,9 +104,13 @@ export default function VideoRoom() {
       // create instance object from token
       const zp = ZegoUIKitPrebuilt.create(token);
 
+      
+      
+
       // start the call
       zp.joinRoom({
         container: element,
+       
         sharedLinks: [
           {
             name: 'Personal link',
@@ -66,6 +126,7 @@ export default function VideoRoom() {
         },
       });
     });
+    
   };
 
   return (
@@ -73,9 +134,14 @@ export default function VideoRoom() {
       <div
         className="myCallContainer"
         ref={myMeeting}
-        style={{ width: '100vw', height: '100vh' }}
+        style={{ width: '90vw', height: '100vh', margin: '0 auto' }}
       ></div>
-      <button style={{'position':'absolute', 'top':0, 'left':0, 'cursor':'pointer', 'backgroundColor':'green', 'border':'none', 'outline':'none', 'fontSize':'1.2rem', 'padding':'0.3rem 0.4rem', 'borderRadius':'1rem'}} onClick={()=> window.location.href='/chat'}>Back</button>
+      {!recording ?
+        <button onClick={startRecording} style={{ 'position': 'absolute', 'width': 'fit-content', 'top': 10, 'right': 10, 'cursor': 'pointer', 'backgroundColor': `${recording ? 'red' : 'green'}`, 'border': 'none', 'outline': 'none', 'fontSize': '0.8rem', 'padding': '0.5rem 0.5rem', 'borderRadius': '1rem' }}>Start Recording</button> :
+        <button onClick={endRecording} style={{ 'position': 'absolute', 'width': 'fit-content', 'top': 10, 'right': 10, 'cursor': 'pointer', 'backgroundColor': `${recording ? 'red' : 'green'}`, 'border': 'none', 'outline': 'none', 'fontSize': '0.8rem', 'padding': '0.5rem 0.5rem', 'borderRadius': '1rem' }}> End Recording</button>}
+      
+      
+      <button style={{ 'position': 'absolute', 'width': 'fit-content', 'top': 10, 'left': 10, 'cursor': 'pointer', 'backgroundColor': 'green', 'border': 'none', 'outline': 'none', 'fontSize': '1.2rem', 'padding': '0.5rem 0.5rem', 'borderRadius': '1rem' }} onClick={() => window.location.href = '/chat'}>Back</button>
     </React.Fragment>
 
   );
